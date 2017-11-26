d93b2d87e9d14376270657c82c6cbe7333766a3e
kubik11 <kubyshkin2@gmail.com>
2017-11-27 00:47:15 +0300

    app

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     
   </head>
   <body>
+    <div class="overlay"></div>
     <header>
         <div class="container-fluid">
           <div class="top-line">
@@ -22,7 +23,7 @@
               <span class="hamburger-inner"></span>
             </span>
           </button>
-          <nav class="nav">
+          <nav class="nav" id="nav">
           <ul class="navigation">
             <li><a href="#">Home</a></li>
             <li><a href="#">Help</a></li>
@@ -50,6 +51,77 @@
         </div>
       </div>
     </section>
+    <section class="forms">
+      <div class="wrap-sign-up">
+        <form action="#" class="sign-up clearfix" method="get">
+          <h2 class="h2">Sign up today and start using our Online Notes App !</h2>
+          <div class="input-groupe">
+            <label for="username" hidden>Username:</label>
+            <input type="text" name="username" placeholder="Username" id="username" class="input">
+
+            <label for="email" hidden>Email:</label>
+            <input type="text" name="email" placeholder="Email" id="email" class="input">
+
+            <label for="password" hidden>Password:</label>
+            <input type="text" name="password" placeholder="Choose a password" id="password" class="input">
+
+            <label for="confirm" hidden>Confirm:</label>
+            <input type="text" name="confirm" placeholder="Confirm a password" id="confirm" class="input">
+          </div>
+          <div class="button-groupe">
+            <input type="submit" name="submit" value="Sign in" class="button sign-in">
+            <input type="button" name="cancel" value="Cancel" class="button">
+          </div> 
+        </form> 
+        <!-- login form -->
+         <form action="#" class="login-form clearfix" id="login-form" method="get">
+          <h2 class="h2">Login:</h2>
+          <div class="input-groupe">
+            <label for="login" hidden>Login:</label>
+            <input type="text" name="login" placeholder="Login" id="login" class="input">
+
+            <label for="login-password" hidden>Password:</label>
+            <input type="password" name="password" placeholder="Password" id="login-password" class="input">
+            <input type="checkbox" name="checkbox"  id="checkbox" class="input">
+            <label for="remember">Remember me</label>
+            <a href="#" class="hint">Forgot password?</a>
+          </div>
+          <div class="button-groupe">
+            <input type="button" name="register" value="Register" class="button register">
+            <input type="submit" name="login" value="Login" class="button login">
+            <input type="button" name="cancel" value="Cancel" class="button">
+          </div> 
+        </form> 
+      <!-- forgot form -->
+         <form action="#" class="forgot-form login-form clearfix" id="forgot-form" method="get">
+          <h2 class="h2">Forgot password? Enter your email address:</h2>
+          <div class="input-groupe">
+            <label for="email" hidden>Email:</label>
+            <input type="text" name="email" placeholder="Login" id="login" class="input">
+          </div>
+          <div class="button-groupe">
+            <input type="button" name="register" value="Register" class="button register">
+            <input type="submit" name="sub" value="Submit" class="button login">
+            <input type="button" name="cancel" value="Cancel" class="button">
+          </div> 
+        </form> 
+      </div>
+    </section>
+    <footer>
+      <div class="container">
+        <div class="footer-content">
+          <div class="row">
+            <div class="col-md-6 offset-md-3 copy">Copyriot @
+              <?php
+                $today = date('Y');
+                echo "$today";
+              ?>
+            </div>
+            <div class="col-md-3 gunter">Gunter</div>
+          </div>
+        </div>
+      </div>
+    </footer>
     <link rel="stylesheet" href="css/style.css">
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- js/app.js
+++ js/app.js
@@ -8,9 +8,19 @@ $(document).ready(function(){
 			$(".nav").fadeOut().fadeIn('slow');
 		}
 	});
-	$( window ).resize(function(e){
-		if(e >= 1200){
-			$(".nav").css('display', 'flex');
+	// resize of browser window
+	window.onresize = resize;
+	function resize(e){
+		if(this.innerWidth >= 992){
+			$("#nav").css({ display: 'flex'});
+			$('.hamburger').removeClass("is-active")
+			console.log('bigger');
+		}else if(this.innerWidth < 992 && !$('.hamburger').hasClass("is-active")){
+			$("#nav").css({ display: 'none'});
 		}
-	});
+		
+	}
+	window.click = function(e){
+		
+	}
 });
